5c9634c1d23c0359154ee12e1d6e0980da603a3b
Xyrille <Xyrille@DESKTOP-PHMBRF7>
2022-09-22 15:02:22 -0600

    Got part one functioning
    Also added the arithmetic calculator

---
+++ src/java/servlets/AgeCalculatorServlet.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Xyrille
+ */
+public class AgeCalculatorServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
+                .forward(request, response);
+    }
+
+ 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String age = request.getParameter("age");
+        int ageNum = Integer.parseInt(age);
+        int ageTotal = ageNum + 1;
+        String message = "Your age next birthday will be " + ageTotal;
+        
+        request.setAttribute("message", message);
+        
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
+                .forward(request, response);
+    }
+}
